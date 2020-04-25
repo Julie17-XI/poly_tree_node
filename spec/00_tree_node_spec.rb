3856a44daa86aaa264414a0dfd4e22cdae398534
Julie17-XI <lizxitang17@qq.com>
2020-04-25 15:50:54 +0800

    untrack spec

--- spec/00_tree_node_spec.rb
+++ spec/00_tree_node_spec.rb
@@ -64,85 +64,85 @@ describe PolyTreeNode do
     end
   end
 
-  describe "#add_child" do
-    it "should pass itself to the child's #parent=" do
-      expect(node3).to receive(:parent=).with(node2)
-      node2.add_child(node3)
-    end
+#   describe "#add_child" do
+#     it "should pass itself to the child's #parent=" do
+#       expect(node3).to receive(:parent=).with(node2)
+#       node2.add_child(node3)
+#     end
 
-    it "doesn't add the child twice" do
-      node2.add_child(node3)
-      expect(node2.children).to eq([node3])
-    end
-  end
+#     it "doesn't add the child twice" do
+#       node2.add_child(node3)
+#       expect(node2.children).to eq([node3])
+#     end
+#   end
 
-  describe "#remove_child" do
-    before do
-      node3.parent = node2
-    end
+#   describe "#remove_child" do
+#     before do
+#       node3.parent = node2
+#     end
 
-    it "should pass nil to the child's #parent=" do
-      expect(node3).to receive(:parent=).with(nil)
-      node2.remove_child(node3)
-    end
+#     it "should pass nil to the child's #parent=" do
+#       expect(node3).to receive(:parent=).with(nil)
+#       node2.remove_child(node3)
+#     end
 
-    it "should raise an error if node is not a child" do
-      expect do
-        node2.remove_child(node1)
-      end.to raise_error
-    end
-  end
-end
+#     it "should raise an error if node is not a child" do
+#       expect do
+#         node2.remove_child(node1)
+#       end.to raise_error
+#     end
+#   end
+# end
 
-describe 'Searchable' do
-  let(:nodes) { ('a'..'g').map { |value| PolyTreeNode.new(value) } }
+# describe 'Searchable' do
+#   let(:nodes) { ('a'..'g').map { |value| PolyTreeNode.new(value) } }
 
-  before do
-    parent_index = 0
-    nodes.each_with_index do |child, index|
-      next if index.zero?
-      child.parent = nodes[parent_index]
-      parent_index += 1 if index.even?
-    end
-  end
+#   before do
+#     parent_index = 0
+#     nodes.each_with_index do |child, index|
+#       next if index.zero?
+#       child.parent = nodes[parent_index]
+#       parent_index += 1 if index.even?
+#     end
+#   end
 
-  shared_examples_for 'search method' do
-    it "should return itself if it contains the value" do
-      expect(nodes.first.send(search_method, 'a')).to equal(nodes.first)
-    end
+#   shared_examples_for 'search method' do
+#     it "should return itself if it contains the value" do
+#       expect(nodes.first.send(search_method, 'a')).to equal(nodes.first)
+#     end
 
-    it "should find descendant" do
-      expect(nodes.first.send(search_method, 'g')).to equal(nodes[6])
-    end
+#     it "should find descendant" do
+#       expect(nodes.first.send(search_method, 'g')).to equal(nodes[6])
+#     end
 
-    it "should return nil when value is not found" do
-      expect(nodes.first.send(search_method, 'x')).to be_nil
-    end
-  end
+#     it "should return nil when value is not found" do
+#       expect(nodes.first.send(search_method, 'x')).to be_nil
+#     end
+#   end
 
-  describe "#dfs" do
-    let(:search_method) { :dfs }
-    it_behaves_like 'search method'
+#   describe "#dfs" do
+#     let(:search_method) { :dfs }
+#     it_behaves_like 'search method'
 
-    it "should take correct path to descendant" do
-      expect(nodes[2]).to_not receive(:dfs)
-      [0, 1, 3, 4].each do |index|
-        expect(nodes[index]).to receive(:dfs).and_call_original.ordered
-      end
-      expect(nodes.first.dfs('e')).to equal(nodes[4])
-    end
-  end
+#     it "should take correct path to descendant" do
+#       expect(nodes[2]).to_not receive(:dfs)
+#       [0, 1, 3, 4].each do |index|
+#         expect(nodes[index]).to receive(:dfs).and_call_original.ordered
+#       end
+#       expect(nodes.first.dfs('e')).to equal(nodes[4])
+#     end
+#   end
 
-  describe "#bfs" do
-    let(:search_method) { :bfs }
-    it_behaves_like 'search method'
+#   describe "#bfs" do
+#     let(:search_method) { :bfs }
+#     it_behaves_like 'search method'
 
-    it "should take correct path to descendant" do
-      expect(nodes[6]).to_not receive(:value)
-      [0, 1, 2, 3, 4, 5].each do |index|
-        expect(nodes[index]).to receive(:value).and_call_original.ordered
-      end
-      expect(nodes.first.bfs('f')).to equal(nodes[5])
-    end
-  end
+#     it "should take correct path to descendant" do
+#       expect(nodes[6]).to_not receive(:value)
+#       [0, 1, 2, 3, 4, 5].each do |index|
+#         expect(nodes[index]).to receive(:value).and_call_original.ordered
+#       end
+#       expect(nodes.first.bfs('f')).to equal(nodes[5])
+#     end
+#   end
 end
